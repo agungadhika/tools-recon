1284ff57d0036faa477d4d7668dbe3d51ae3c257
Ikan tongkol <ikantongkol@pop-os.localdomain>
2023-10-29 00:29:46 +0800

    penyederhanaan, nanti tinggal di update lagi buat enum subdomain yang diinput

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import requests
 
-def cek_git_directory(url):
-    # Tambahkan '/' ke URL jika tidak ada
+def check_file_existence(url, filename):
     if not url.endswith('/'):
         url += '/'
-
-    # Gabungkan URL dengan .git
-    git_url = url + 'Dockerfile'
-
-    # Kirim permintaan HTTP GET ke URL .git
-    response = requests.get(git_url)
-
-    # Periksa status respons untuk menentukan apakah .git ada
+    file_url = url + filename
+    response = requests.head(file_url)
     if response.status_code == 200:
-        print(f"File Docker ditemukan di {url}")
+        print(f"File {filename} ditemukan di {url}")
     else:
-        print(f"File Docker tidak ditemukan di {url}")
+        print(f"File {filename} tidak ditemukan di {url}")
 
 if __name__ == "__main__":
     website_url = input("Masukkan URL website: ")
-    cek_git_directory(website_url)
+    check_file_existence(website_url, 'Dockerfile')
+    check_file_existence(website_url, 'docker-compose.yml')
+    check_file_existence(website_url, 'nginx.conf')
+
+
+# Buatkan untuk nginx.conf, default.conf, .git, docker-compose.yml
+#.gitkeep
+#.git-rewrite
+#.gitreview
+#.git/HEAD
+#.git/config
+#.git/index
+#.git/logs
+#.svnignore
+#.gitattributes
+#.gitmodules
+#.svn/entries
